8984a2f972dc4dca0f777a11d1257686de26e6b9
Mahmoud Mamdouh <11410120210376@stud.cu.edu.eg>
2025-05-06 10:09:48 +0000

    added MNIST dataset

--- data_utils.py
+++ data_utils.py
@@ -11,6 +11,14 @@ def get_cifar10():
     test_data = datasets.CIFAR10('./data', train=False, download=True, transform=transform)
     return train_data, test_data
 
+def get_mnist():
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # Convert images to PyTorch tensors
+        transforms.Normalize((0.1307,), (0.3081,))  # Normalize with MNIST mean and std
+    ])
+    train_data = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    test_data = datasets.MNIST('./data', train=False, download=False, transform=transform)
+    return train_data, test_data
 
 def get_dataloader(train_data, indices, batch_size=64, shuffle=True):
     subset = Subset(train_data, indices)
